796105e4a7c63051fa0afc08d45c51a47249e0d1
Anish T <anishthjune2006@gmail.com>
2026-01-29 22:45:36 +0530

    Prepare for Render Deployment

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const multer = require('multer');
 
 // --- FIREBASE SETUP ---
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    : require('./serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
